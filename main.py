0944f56657e06e2dbe60c9573fa9608badd35a67
Ian Bell <ian.bell@nist.gov>
2019-03-25 16:42:23 -0600

    Don't use session, better error handling with JSON

--- main.py
+++ main.py
@@ -5,14 +5,32 @@ import json
 import time
 import timeit
 import sys
+import traceback
+import uuid
 
 # Flask-y things
 from flask import Flask, request, jsonify, current_app, url_for, render_template, make_response, session
+from flask_sqlalchemy import SQLAlchemy
+
 # Other libraries
 import requests
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.secret_key = os.urandom(32)
+
+# Flask-SQLAlchemy setup
+# ----------------------
+if os.path.exists('test.db'):
+    os.remove('test.db')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:' # See this: https://gehrcke.de/2015/05/in-memory-sqlite-database-and-flask-a-threading-trap/
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+class RepoData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    session_id = db.Column(db.String)
+    contents = db.Column(db.String)
+db.create_all()
 
 ##########################################################
 ##################     ROUTES     ########################
@@ -20,19 +38,25 @@ app.secret_key = os.urandom(32)
 
 the_repos = ['usnistgov/REFPROP-issues','usnistgov/REFPROP-wrappers','usnistgov/REFPROP-manager','usnistgov/REFPROP-cmake']
 if sys.platform.startswith('win'):
-    HOME = 'Q:/IHB/issues_notes'
+    HOME = 'Q:/Public/IHB/issues_notes'
 else:
     HOME = '/media/Q/IHB/issues_notes'
 
 def api_calls(session):
-    for repo in the_repos:
-        if repo not in session:
-            session[repo] = requests.get('https://api.github.com/repos/'+repo+'/issues?state=open').json()
+    repoze = {}
+    with requests.Session() as rs:
+        pat = open('gh_pat','r').read()
+        for repo in the_repos:
+            if repo not in session:
+                repoze[repo] = rs.get('https://api.github.com/repos/'+repo+'/issues?state=open', auth=('ianhbell', pat)).json()
+    db.session.add(RepoData(session_id=session['Id'], contents=json.dumps(repoze)))
 
 def get_assignees(issue):
     return [a['login'] for a in issue['assignees']]
 
 def attach_notes(items):
+    if not os.path.exists(HOME):
+        raise ValueError("HOME doesn't exist: " + HOME)
     notes = {}
     for repo in the_repos:
         path = os.path.join(HOME, repo.replace('/','_'))
@@ -55,22 +79,31 @@ def attach_notes(items):
 
 def get_items(session):
     items = []
+    repoze = json.loads(RepoData.query.filter_by(session_id=session['Id']).first().contents)
     for repo in the_repos:
-        for issue in session[repo]:
+        for issue in repoze[repo]:
             items.append({
                 'repo': repo,
-                'repo_short': repo.split('/')[1],
+                'repo_short': repo.split('/')[1].split('-')[1],
                 'issue_num': issue['number'],
                 'title': issue['title'],
                 'assignees': ','.join(get_assignees(issue))
             })
-    items = attach_notes(items)
     return items
 
 @app.route('/')
 def get_open_issues():
+    session['Id'] = str(uuid.uuid1())
     api_calls(session)
-    return render_template('frontend.html', items=get_items(session))
+    log_message = ''
+    items = get_items(session)
+    try:
+        items = attach_notes(items)
+    except BaseException as BE:
+        log_message = traceback.format_exc()
+        print(BE)
+    print(log_message)
+    return render_template('frontend.html', items=items, log_message=log_message)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)#, ssl_context=('cert.pem', 'key.pem'))
